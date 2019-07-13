9570800ccb0b3a471e86c74b47cf3a5d8fcb8edd
Matthew Flood <hermannwest@gmail.com>
2019-07-13 06:30:42 -0600

    Make sure we setup in a python3 env

--- app/setup.sh
+++ app/setup.sh
@@ -1,3 +1,11 @@
+
+
+which python3
+if [ "$?" == "1" ]; then
+    echo "python3 is not available"
+    exit 1
+fi
+
 virtualenv venv -p python3
 source venv/bin/activate
 pip install -r requirements.txt
