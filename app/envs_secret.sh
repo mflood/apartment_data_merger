d1900c8ec74599eb9f8ca2de5a81bddf887757b1
Matthew Flood <hermannwest@gmail.com>
2019-07-15 05:36:55 -0600

    add secrets

---
+++ app/envs_secret.sh
@@ -0,0 +1,21 @@
+export MYSQL_HOST=localhost
+export MYSQL_PORT=3306
+export MYSQL_DATABASE=coursera
+export MYSQL_USERNAME=root
+export MYSQL_PASSWORD=
+
+export POSTGRES_HOST=digible2.cyhyspsglw2i.us-east-1.rds.amazonaws.com
+export POSTGRES_PORT=5432
+export POSTGRES_DATABASE=digible
+export POSTGRES_USERNAME=digible_master
+export POSTGRES_PASSWORD=
+
+export POSTGRES_HOST=localhost
+export POSTGRES_PORT=5432
+export POSTGRES_DATABASE=postgres
+export POSTGRES_USERNAME=postgres
+export POSTGRES_PASSWORD=
+
+export SNOWFLAKE_TABLE=digible_schema.snowflake_table
+export SQLSERVER_TABLE=digible_schema.sqlserver_table
+export MAPPING_TABLE=digible_schema.mapping_table
