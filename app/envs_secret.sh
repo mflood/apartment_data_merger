b711c30f6deb43b0767de61474ec9096544b2271
Matthew Flood <hermannwest@gmail.com>
2019-07-15 09:20:50 -0600

    Use real data instead of sample

--- app/envs_secret.sh
+++ app/envs_secret.sh
@@ -7,7 +7,7 @@ export MYSQL_PASSWORD=
 export POSTGRES_HOST=localhost
 export POSTGRES_PORT=5432
 export POSTGRES_DATABASE=postgres
-export POSTGRES_USERNAME=postgres
+export POSTGRES_USERNAME=app_db_user
 export POSTGRES_PASSWORD=fixme
 
 # used for full and sample
@@ -16,8 +16,8 @@ export MAPPING_TABLE=digible_schema.mapping_table
 # full set
 export SNOWFLAKE_FILE=../data/snowflake_table.txt
 export SQLSERVER_FILE=../data/sqlserver_table.txt
-export SNOWFLAKE_TABLE=digible_schema.snowflake_table_sample
-export SQLSERVER_TABLE=digible_schema.sqlserver_table_sample
+export SNOWFLAKE_TABLE=digible_schema.snowflake_table
+export SQLSERVER_TABLE=digible_schema.sqlserver_table
 
 # sample set:
 # export SNOWFLAKE_FILE=../data/snowflake_table_sample.txt
