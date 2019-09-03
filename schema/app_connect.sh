13dad4ccca0f3d7ac33006cd0e1a22ed0aadec0c
Matthew Flood <hermannwest@gmail.com>
2019-09-02 18:09:41 -0600

    Add connect scripts

---
+++ schema/app_connect.sh
@@ -0,0 +1,7 @@
+USER=app
+HOST=digible2.cyhyspsglw2i.us-east-1.rds.amazonaws.com
+DBNAME=digible
+/Applications/Postgres.app/Contents/Versions/10/bin/psql --port=5432 --host=$HOST -U $USER --password --dbname=$DBNAME 
+
+
+
---
+++ schema/master_connect.sh
@@ -0,0 +1,8 @@
+cat secrets.txt
+USER=digible_master
+HOST=digible2.cyhyspsglw2i.us-east-1.rds.amazonaws.com
+DBNAME=digible
+/Applications/Postgres.app/Contents/Versions/10/bin/psql --port=5432 --host=$HOST -U $USER --password --dbname=$DBNAME 
+
+
+
